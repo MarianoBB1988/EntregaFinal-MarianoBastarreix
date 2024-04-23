6e4f3554f72b615b1adb57161116712859c6a778
Mariano Bastarreix Blanco <110568969+MarianoBB1988@users.noreply.github.com>
2024-04-23 14:48:32 -0300

    Update CarrouselComponent.jsx netlify

--- src/components/home/CarrouselComponent.jsx
+++ src/components/home/CarrouselComponent.jsx
@@ -11,13 +11,13 @@ export default function CarrouselComponent() {
 
                     <MDBCarousel showControls interval={2000} className='d-block w-100 h-100'>
                         <MDBCarouselItem itemId={1}>
-                            <img src='https://raw.githubusercontent.com/MarianoBB1988/EntregaFinal-MarianoBastarreix/main/src/assets/banner1.gif'} style={{width:'100%'}}  alt='...' />
+                             <img src={'https://raw.githubusercontent.com/MarianoBB1988/EntregaFinal-MarianoBastarreix/main/src/assets/banner1.gif'} style={{width:'100%'}}  alt='...' />
                         </MDBCarouselItem>
                         <MDBCarouselItem itemId={2}>
-                        <img src={'https://raw.githubusercontent.com/MarianoBB1988/EntregaFinal-MarianoBastarreix/main/src/assets/banner2.gif'} style={{width:'100%'}}  alt='...' />
+                             <img src={'https://raw.githubusercontent.com/MarianoBB1988/EntregaFinal-MarianoBastarreix/main/src/assets/banner2.gif'} style={{width:'100%'}}  alt='...' />
                         </MDBCarouselItem>
                         <MDBCarouselItem itemId={3}>
-                        <img src={'https://raw.githubusercontent.com/MarianoBB1988/EntregaFinal-MarianoBastarreix/main/src/assets/banner3.gif'} style={{width:'100%'}}  alt='...' />
+                             <img src={'https://raw.githubusercontent.com/MarianoBB1988/EntregaFinal-MarianoBastarreix/main/src/assets/banner3.gif'} style={{width:'100%'}}  alt='...' />
                         </MDBCarouselItem>
                     </MDBCarousel>
 
